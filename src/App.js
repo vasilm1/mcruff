4574d17245413e262ccce55aed10bc0d8175e2fc
Vasil <58887500+vasilm1@users.noreply.github.com>
2024-07-01 01:53:31 -0500

    yas

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home/Home.js';
 import Menu from './components/Menu/Menu.js';
-import {Route} from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-
-    <div className="App">
-        <Home />
-         <Route path='/menu' element={<Menu/>}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Home.css';
 import Navbar from '../Navbar.js';
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import {useEffect} from "react";
+
 
 import mcruff from './assets/mcruff.png'
 import bubble from './assets/bubble.png'
@@ -10,18 +14,29 @@ import the from './assets/The.png'
 import menu from './assets/Menu.png'
 
 function Home() {
+
+    useEffect(() => {
+        AOS.init({
+            duration:2000,
+            delay: 100,
+        })
+    }, []);
+
   return (
 
       <div className="Home">
           <Navbar/>
           <div className="hello">
-              <img src={mcruff} className="mcruffin"/>
-              <img src={bubble} className="bubble"/>
-              <img src={check} className="check"/>
-              <img src={the} className="the"/>
-              <img src={menu} className="menu"/>
 
-          </div>
+              <img src={mcruff} className="mcruffin" data-aos="slide-right"/>
+              <img src={bubble} className="bubble" data-aos="slide-right"/>
+              <img src={check} className="check" data-aos="slide-right"/>
+              <img src={the} className="the" data-aos="slide-right"/>
+              <a href="https://mcruff.org/menu" data-aos="slide-right">
+                  <img src={menu} className="menu"/>
+              </a>
+
+              </div>
       </div>
   );
 }
